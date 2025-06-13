d53a9cedc4e0eb8ea2a23416c1b6380850927fb1
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2025-06-13 14:02:09 -0500

    feat: add CourseMappingProfile - Implement AutoMapper profile for Course entity and DTO mappings

---
+++ dotnet-oev-backend-rest/Mappers/CourseMappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using dotnet_oev_backend_rest.Dtos.Request;
+using dotnet_oev_backend_rest.Dtos.Response;
+using dotnet_oev_backend_rest.Models;
+
+namespace dotnet_oev_backend_rest.Mappers;
+
+public class CourseMappingProfile : Profile
+{
+    public CourseMappingProfile()
+    {
+        // Mapeo de Entidad a DTO de Respuesta
+        CreateMap<Course, CourseResponseDTO>()
+            .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.User.Id))
+            .ForMember(dest => dest.InstructorName,
+                opt => opt.MapFrom(src => $"{src.User.Name} {src.User.PaternalSurname}"));
+
+        // Mapeo de DTO de Creación a Entidad
+        CreateMap<CourseRequestDTO, Course>();
+
+        // Mapeo de DTO de Actualización a Entidad
+        CreateMap<UpdateCourseRequestDTO, Course>()
+            .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+    }
+}
