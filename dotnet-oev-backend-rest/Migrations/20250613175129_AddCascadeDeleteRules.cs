37debc740cf463c2b1256c490a2e1e2d6c1d396f
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2025-06-13 13:01:35 -0500

    feat: add migration for cascade delete rules - Update foreign key constraints for Course and User relationships

---
+++ dotnet-oev-backend-rest/Migrations/20250613175129_AddCascadeDeleteRules.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_oev_backend_rest.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCascadeDeleteRules : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tbl_course_tbl_user_UserId",
+                table: "tbl_course");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tbl_course_tbl_user_UserId",
+                table: "tbl_course",
+                column: "UserId",
+                principalTable: "tbl_user",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tbl_course_tbl_user_UserId",
+                table: "tbl_course");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tbl_course_tbl_user_UserId",
+                table: "tbl_course",
+                column: "UserId",
+                principalTable: "tbl_user",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ dotnet-oev-backend-rest/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_oev_backend_rest.Repositories.Interfaces;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<T?> GetByIdAsync(long id);
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task<T> AddAsync(T entity);
+    void Update(T entity); // No es async
+    void Delete(T entity); // No es async
+}
