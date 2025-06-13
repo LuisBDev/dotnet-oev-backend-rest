c46f1c16f078fda76610be9b8fadcc5c17c09c45
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2025-06-13 13:03:43 -0500

    feat: implement generic repository pattern - Add GenericRepository class for CRUD operations

---
+++ dotnet-oev-backend-rest/Repositories/Implementations/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using dotnet_oev_backend_rest.Data;
+using dotnet_oev_backend_rest.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_oev_backend_rest.Repositories.Implementations;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    private readonly ApplicationDbContext _context;
+
+    public GenericRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        await _context.Set<T>().AddAsync(entity);
+        // El guardado se hará en una "Unidad de Trabajo", no aquí.
+        return entity;
+    }
+
+    public void Delete(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+    }
+
+    public async Task<IReadOnlyList<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public async Task<T?> GetByIdAsync(long id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+
+    public void Update(T entity)
+    {
+        // Attach asegura que EF Core rastree la entidad y su estado "Modified"
+        _context.Set<T>().Attach(entity);
+        _context.Entry(entity).State = EntityState.Modified;
+    }
+}
