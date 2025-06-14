4c063b02d0f5bf2da88a373815570465ba3fbe58
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2025-06-14 13:37:16 -0500

    feat: add IUserService and DTOs for user management

---
+++ dotnet-oev-backend-rest/Dtos/Request/UpdateUserRequestDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace dotnet_oev_backend_rest.Dtos.Request;
+
+public class UpdateUserRequestDTO
+{
+    public long? Id { get; set; }
+    public string? Name { get; set; }
+    public string? PaternalSurname { get; set; }
+    public string? MaternalSurname { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+}
---
+++ dotnet-oev-backend-rest/Dtos/Response/UserResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace dotnet_oev_backend_rest.Dtos.Response;
+
+public class UserResponseDTO
+{
+    
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string PaternalSurname { get; set; }
+    public string MaternalSurname { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Role { get; set; }
+    
+    
+}
