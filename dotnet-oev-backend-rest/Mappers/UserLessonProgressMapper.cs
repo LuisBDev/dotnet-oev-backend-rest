eafe742e966ee90541aa91d02efef683e4db7b59
Milko024 <marlondurand675@gmail.com>
2025-06-28 19:08:36 -0500

    add:implementing lessons functions for teacher

---
+++ dotnet-oev-backend-rest/Mappers/UserLessonProgressMapper.cs
@@ -0,0 +1,16 @@
+using dotnet_oev_backend_rest.Models.Enums;
+
+namespace dotnet_oev_backend_rest.Dtos.Response
+{
+    public class UserLessonProgressResponseDTO
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public long LessonId { get; set; }
+        public string LessonTitle { get; set; } = string.Empty;
+        public string LessonVideoKey { get; set; } = string.Empty;
+        public int Duration { get; set; }
+        public Status Status { get; set; }
+        public DateTime CompletedAt { get; set; }
+    }
+}
