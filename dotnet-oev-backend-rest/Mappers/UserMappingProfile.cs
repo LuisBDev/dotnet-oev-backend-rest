344b43a560a470e3e18d6a8bdbd60eafb9182209
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2025-07-06 22:00:44 -0500

    refactor(mapping): simplify User mapping configurations by removing ignored members

--- dotnet-oev-backend-rest/Mappers/UserMappingProfile.cs
+++ dotnet-oev-backend-rest/Mappers/UserMappingProfile.cs
@@ -10,14 +10,7 @@ public class UserMappingProfile : Profile
     public UserMappingProfile()
     {
         // Mapeo de UserRegisterRequestDTO a User
-        CreateMap<UserRegisterRequestDTO, User>()
-            .ForMember(dest => dest.Id, opt => opt.Ignore())
-            .ForMember(dest => dest.Password, opt => opt.Ignore())
-            .ForMember(dest => dest.ProfilePicture, opt => opt.Ignore())
-            .ForMember(dest => dest.Role, opt => opt.Ignore())
-            .ForMember(dest => dest.EnrollmentList, opt => opt.Ignore())
-            .ForMember(dest => dest.CourseList, opt => opt.Ignore())
-            .ForMember(dest => dest.LessonProgressList, opt => opt.Ignore());
+        CreateMap<UserRegisterRequestDTO, User>();
 
         // Mapeo de User a UserResponseDTO
         CreateMap<User, UserResponseDTO>()
@@ -25,20 +18,14 @@ public class UserMappingProfile : Profile
 
 
         // Mapeo de UpdateUserRequestDTO a User
-        var updateMap = CreateMap<UpdateUserRequestDTO, User>()
+        CreateMap<UpdateUserRequestDTO, User>()
             .ForMember(dest => dest.Id, opt => opt.Ignore())
-            .ForMember(dest => dest.Password, opt => opt.Ignore())
-            .ForMember(dest => dest.ProfilePicture, opt => opt.Ignore())
-            .ForMember(dest => dest.Role, opt => opt.Ignore())
-            .ForMember(dest => dest.EnrollmentList, opt => opt.Ignore())
-            .ForMember(dest => dest.CourseList, opt => opt.Ignore())
-            .ForMember(dest => dest.LessonProgressList, opt => opt.Ignore());
-        updateMap.ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
 
         // Mapeo de User a AuthUserResponseDTO
         CreateMap<User, AuthUserResponseDTO>()
             .ForMember(dest => dest.Token, opt => opt.Ignore()) // Token se asigna manualmente
             .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role.ToString().ToUpper()));
-        // ...existing code...
+        ;
     }
 }
